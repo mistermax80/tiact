df1fbe13880d37d329458c5ad9edae1aae5ffd91
giglimassimo <giglimassimo@gmail.com>
2010-12-16 16:37:52 +0000

    - Aggiunta classi per la generazione della persistenza (da provare con hsqldb)

---
+++ Time/src/tiact/exception/PersistenceException.java
@@ -0,0 +1,26 @@
+package tiact.exception;
+
+public class PersistenceException extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7722423773888974482L;
+
+	public PersistenceException() {
+		super();
+	}
+
+	public PersistenceException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+	}
+
+	public PersistenceException(String arg0) {
+		super(arg0);
+	}
+
+	public PersistenceException(Throwable arg0) {
+		super(arg0);
+	}
+
+}
